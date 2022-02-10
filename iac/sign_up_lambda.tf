24d6be3d394ed0d2a5ff23f79cf7db34cc958189
Andre Torres <atorres@hagerty.com>
2022-02-10 11:47:36 -0500

    added lambda permission to sign_up lambda

--- iac/sign_up_lambda.tf
+++ iac/sign_up_lambda.tf
@@ -1,7 +1,12 @@
 locals {
+  sign_up_lambda_policy = templatefile("./policies/sign_up_lambda_policy.json", {
+    HAMPI_TABLE_ARN = aws_dynamodb_table.hampi_table.arn
+  })
+
   sign_up_environment_variables = {
     AWS_NODEJS_CONNECTION_REUSE_ENABLED = var.AWS_NODEJS_CONNECTION_REUSE_ENABLED
     NODE_ENV                            = "production"
+    HAMPI_APP_TABLE                     = aws_dynamodb_table.hampi_table.name
   }
 
   sign_up_lambda_name = "hampi_sign_up_lambda"
@@ -17,6 +22,12 @@ module "sign_up_lambda" {
   aws_region       = local.region
   environment_vars = local.sign_up_environment_variables
   memory_size      = var.LAMBDA_DEFAULT_MEMORY_SIZE
+}
+
+resource "aws_iam_role_policy" "sign_up_lambda_function_role_policy" {
+  name   = "hampi_sign_up_${local.namespace}_lambda_function_policy"
+  role   = module.sign_up_lambda.iam_role_name
+  policy = local.sign_up_lambda_policy
 }
 
 # API Gateway lambda invoke permission
